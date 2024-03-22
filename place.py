a2692c5b30280d274c5a0db1e27c4015efc8659d
wrains54 <160513216+wrains54@users.noreply.github.com>
2024-03-22 09:59:24 -0500

    Add files via upload

--- place.py
+++ place.py
@@ -5,3 +5,8 @@ root = tk.Tk()
 root.title("Login Page")
 root.geometry("300x200")
 
+username_label = ttk.Label(root, text="Username:")
+username_label.place(x=50, y=50, width=100, height=25)
+
+username_entry = ttk.Entry(root)
+username_entry.place(x=150, y=50, width=100, height=25)
